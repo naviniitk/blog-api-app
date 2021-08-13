9a3ff0dd8d200d276a8afff165865c1dfaca2e15
Naveen Kumar <navin10102001@gmail.com>
2021-08-13 16:01:48 +0530

    models prepared

--- models/post.js
+++ models/post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const PostSchema = new Schema({
+const Post = new Schema({
   title: {
     type: String,
     required: true,
